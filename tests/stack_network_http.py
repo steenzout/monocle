5b65a512997c25e3c5ba76c5f763b4c60388e10f
Steven Hazel <sah@saucelabs.com>
2015-11-01 20:42:48 -0800

    regression test for a past bug with large request bodies

--- tests/stack_network_http.py
+++ tests/stack_network_http.py
@@ -7,15 +7,17 @@ from monocle.stack.network import http
 
 
 @contextmanager
-def http_server_running(port):
+def http_server_running(port, handler=None):
     @_o
-    def handler(conn):
+    def _handler(conn):
         data = 'Hello, World!'
         headers = http.HttpHeaders()
         headers.add('Content-Length', len(data))
         headers.add('Content-Type', 'text/plain')
         headers.add('Connection', 'close')
         yield Return(200, headers, data)
+    if not handler:
+        handler = _handler
     service = http.HttpServer(port, handler=handler)
     network.add_service(service)
     try:
@@ -43,3 +45,27 @@ def test_client():
             yield client.connect(addr, port)
             r = yield client.request('/')
             assert r.code == '200'  # should this be an int?
+
+
+@test
+@_o
+def test_large_requests():
+    addr = '127.0.0.1'
+    port = 5557
+    recv_body = []
+
+    @_o
+    def _handler(conn):
+        recv_body.append(conn.body_file.read())
+        data = 'Hello, World!'
+        headers = http.HttpHeaders()
+        headers.add('Content-Length', len(data))
+        headers.add('Content-Type', 'text/plain')
+        headers.add('Connection', 'close')
+        yield Return(200, headers, data)
+    with http_server_running(port, handler=_handler):
+        with http_client() as client:
+            yield client.connect(addr, port)
+            send_body = 'x' * 1024 * 1024 * 10
+            r = yield client.request('/', method='POST', body='x' * 1024 * 1024 * 10)
+            assert recv_body[0] == send_body
