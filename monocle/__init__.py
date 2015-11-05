f8c5c40bc134b96f782c487d45ad084c5d228153
Michael Ricks-Aherne <miketwo@saucelabs.com>
2015-11-05 13:39:16 -0800

    Better importing for dev use
     - More explicit error message
     - Allow setting the stack in the environment
     - gitignore products of `pip install -e .`

--- monocle/__init__.py
+++ monocle/__init__.py
@@ -3,7 +3,7 @@ import sys
 import core
 from core import _o, o, launch, Return, InvalidYieldException, log_exception
 
-VERSION = '0.35'
+VERSION = '0.36'
 
 _stack_name = None
 def init(stack_name):
--- monocle/stack/eventloop.py
+++ monocle/stack/eventloop.py
@@ -1,7 +1,13 @@
 import monocle
-if monocle._stack_name == 'twisted':
+import os
+if monocle._stack_name == 'twisted' or os.getenv('MONOCLE_STACK') == 'twisted':
     from monocle.twisted_stack.eventloop import *
-elif monocle._stack_name == 'tornado':
+elif monocle._stack_name == 'tornado' or os.getenv('MONOCLE_STACK') == 'tornado':
     from monocle.tornado_stack.eventloop import *
-elif monocle._stack_name == 'asyncore':
+elif monocle._stack_name == 'asyncore' or os.getenv('MONOCLE_STACK') == 'asyncore':
     from monocle.asyncore_stack.eventloop import *
+else:
+    raise ImportError(
+        "Could not import stack.\n"
+        "Ensure you have called monocle.init()\n"
+        "or defined MONOCLE_STACK=<stack> in the environment")
